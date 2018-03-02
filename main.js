cbc7a23212fde1e53047ef4d57ed50b4878fb770
Aosanders <sanderscross@gmail.com>
2018-03-02 11:44:16 -0800

    minor changes

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-const clickEvents = [];
-const runBtn = document.querySelector(".run-btn");
 
-document.addEventListener("click", function(e){
-    let clickObj = {
-        xCord: e.clientX,
-        yCord: e.clientY
-    };
-    clickEvents.push(clickObj);
-});
 
-runBtn.addEventListener("click", function(){
-    let bubble = document.createElement("div");
-    bubble.classList.add("bubbleElem");
-    document.appendchild(bubble);
+    const clickEvents = [];
+
+    document.addEventListener("click", function(e){
+        let clickObj = {
+            xCord: e.clientX,
+            yCord: e.clientY
+        };
+        clickEvents.push(clickObj);
+            //console.log(runBtn);
+            console.log(clickObj.xCord);
+    });
+
+document.addEventListener("DOMcontentLoaded",function(){
+    const runBtn = document.querySelector(".run-btn");
+
+    runBtn.addEventListener("click", function(){
+        let bubble = document.createElement("div");
+        bubble.classList.add("bubbleElem");
+        document.appendchild(bubble);
+    });
 });
